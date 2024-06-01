584be3876f113097db6f433f398fef6c9f7241a0
Aleksei Zarubin <12220926+alexzarbn@users.noreply.github.com>
2024-06-01 20:17:35 +0500

    chore: initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,52 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: ['@nuxt/ui-pro'],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/fonts',
+    '@nuxthq/studio',
+    'nuxt-og-image'
+  ],
+  hooks: {
+    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
+    'components:extend': (components) => {
+      const globals = components.filter(c => ['UButton', 'UIcon'].includes(c.pascalName))
+
+      globals.forEach(c => c.global = true)
+    }
+  },
+  ui: {
+    icons: ['heroicons', 'simple-icons']
+  },
+  css: [
+    'assets/css/main.css'
+  ],
+  content: {
+    highlight: {
+      langs: ['json', 'js', 'ts', 'html', 'css', 'vue', 'shell', 'mdc', 'md', 'yaml', 'php']
+    }
+  },
+  colorMode: {
+    disableTransition: true,
+    preference: 'dark'
+  },
+  routeRules: {
+    '/api/search.json': { prerender: true }
+  },
+  devtools: {
+    enabled: true
+  },
+  typescript: {
+    strict: false
+  },
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
